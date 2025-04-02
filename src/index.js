86f19e6c4eedd7eb0436a234bcba977b4752f200
Juan Cristobal <65052633+juancristobalgd1@users.noreply.github.com>
2025-04-02 04:16:35 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,54 +1,33 @@
-const template = document.createElement("template");
-template.innerHTML = /*html*/`
-  <style>
-    * {
-      font-size: 200%;
-    }
+ 
+  import { component, html, signal } from "https://unpkg.com/esor/dist/esor.min.js";
 
-    span {
-      width: 4rem;
-      display: inline-block;
-      text-align: center;
-    }
+component('my-counter', () => {
+  const count = signal(0);
 
-    button {
-      width: 4rem;
-      height: 4rem;
-      border: none;
-      border-radius: 10px;
-      background-color: seagreen;
-      color: white;
-    }
-  </style>
-  <button id="dec">-</button>
-  <span id="count"></span>
-  <button id="inc">+</button>`;
+  return html`
+      <button onclick=${()=>count(count() - 1)}>-</button>
+      <span>${count}</span>
+      <button onclick=${()=>count(count() + 1)}>+</button>
 
-class MyCounter extends HTMLElement {
-  constructor() {
-    super();
-    this.count = 0;
-    this.attachShadow({ mode: "open" });
-  }
-
-  connectedCallback() {
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
-    this.shadowRoot.getElementById("inc").onclick = () => this.inc();
-    this.shadowRoot.getElementById("dec").onclick = () => this.dec();
-    this.update(this.count);
-  }
-
-  inc() {
-    this.update(++this.count);
-  }
-
-  dec() {
-    this.update(--this.count);
-  }
-
-  update(count) {
-    this.shadowRoot.getElementById("count").innerHTML = count;
-  }
-}
-
-customElements.define("my-counter", MyCounter);
+      <style>
+        * {
+          font-size: 200%;
+        }
+    
+        span {
+          width: 4rem;
+          display: inline-block;
+          text-align: center;
+        }
+    
+        button {
+          width: 4rem;
+          height: 4rem;
+          border: none;
+          border-radius: 10px;
+          background-color: seagreen;
+          color: white;
+        }
+      </style>
+  `;
+});
